95dd3c89f31fac7249daa4ad3a612e7ab8ddfac8
Pedro Martinez <pedro@dkltd.net>
2025-02-05 12:17:52 +0100

    Add project files.

---
+++ ServerManagement/Models/CitiesRepos.cs
@@ -0,0 +1,16 @@
+﻿namespace ServerManagement.Models
+{
+    public static class CitiesRepos
+    {
+        private static List<String> cities = new List<String> 
+        {
+            "Toronto", 
+            "Montreal", 
+            "Ottawa", 
+            "Calgary", 
+            "Halifax" 
+        };
+
+        public static List<String> GetCities() => cities;
+    }
+}
